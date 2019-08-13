8e2ebaa3b9f6fa52b90ac0a5d878358de8081a9a
omadrigals <47482766+omadrigals@users.noreply.github.com>
2019-08-12 21:32:41 -0600

    Login
    Login

--- MulticentroProyectoFinal/GUI/Login.cs
+++ MulticentroProyectoFinal/GUI/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace MulticentroProyectoFinal
 {
     public partial class Login : Form
     {
+        ConexionesBasicasAbrirCerrarBD con;
         public Login()
         {
             InitializeComponent();
+            con = new ConexionesBasicasAbrirCerrarBD();
         }
 
         private void BtnSalirLogin_Click(object sender, EventArgs e)
@@ -24,15 +27,42 @@ namespace MulticentroProyectoFinal
 
         private void BtnIngresarLogin_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            MenuPrincipal menuPrincipalPrograma = new MenuPrincipal();
-            menuPrincipalPrograma.Show();
-         }
+            string userid = txtUsuarioLogin.Text;
+            string password = txtPasswordLogin.Text;
+
+            con.AbrirConexion();
+
+            SqlCommand cmd = new SqlCommand("SELECT empleadoid FROM empleado WHERE userid = '" + userid + "' AND password = '" + password + "'",con.GetSqlConnection());
+            cmd.Parameters.AddWithValue("@UserName", userid);
+            cmd.Parameters.AddWithValue("@Password", password);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            
+            DataTable dt = new DataTable();
+
+            var x = cmd.Connection.DataSource;
+
+            da.Fill(dt);
+
+            con.CerrarConexion(); 
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login invalido por favor revise su usuario o su contraseña");
+            }
+            else
+            {   
+                this.Hide();
+                MenuPrincipal menuPrincipalPrograma = new MenuPrincipal();
+                menuPrincipalPrograma.Show();
+            }
+
+
+
+        }
 
         private void Login_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
         }
-
     }
 }
